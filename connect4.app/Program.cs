f653f649ff09af6e2b3c2f244c06d859acf389b9
majbo <codingmajbo@gmail.com>
2018-01-21 01:17:24 +0100

    Created projects connect4.app and connect4.lib including demo call

---
+++ connect4.app/Program.cs
@@ -0,0 +1,20 @@
+﻿using connect4.lib;
+using System;
+
+namespace connect4.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var board1 = new Board();
+            board1.States[37] = BoardCellState.Blue;
+            board1.States[38] = BoardCellState.Red;
+
+            Console.WriteLine(board1);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ connect4.lib/Board.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace connect4.lib
+{
+    public enum BoardCellState
+    {
+        Empty,
+        Blue,
+        Red
+    }
+
+    public class Board
+    {
+        private BoardCellState[] _states = new BoardCellState[42];
+
+        public BoardCellState[] States { get => _states; }
+
+
+        public override string ToString()
+        {
+            string s = "";
+            int index;
+            for (int row = 0; row < 6; row++)
+            {
+                s += Environment.NewLine;
+                s += "--------------";
+                s += Environment.NewLine;
+                for (int col = 0; col < 7; col++)
+                {
+                    index = row * 7 + col;
+                    switch(_states[index])
+                    {
+                        case BoardCellState.Empty:
+                            s += "| ";
+                            break;
+                        case BoardCellState.Blue:
+                            s += "|X";
+                            break;
+                        case BoardCellState.Red:
+                            s += "|O";
+                            break;
+                    }
+                }
+      
+
+            }
+            s += Environment.NewLine;
+            s += "--------------";
+
+            return s;
+        }
+    }
+}
