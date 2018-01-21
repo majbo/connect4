6c89ce984de7960706b554a751ece0c6ed0d54f6
majbo <codingmajbo@gmail.com>
2018-01-21 22:44:06 +0100

    Added unit tests for analyzer

---
+++ connect4.test/Analyzer/HasWonTest.cs
@@ -0,0 +1,127 @@
+﻿using connect4.lib;
+using System.Collections.Generic;
+using Xunit;
+
+namespace connect4.test
+{
+    public class HasWonTest
+    {
+        private static readonly BoardCellState WinnerColor = BoardCellState.Blue;
+
+        public static IEnumerable<object[]> GetHorizontallyWinnerBoards()
+        {
+            var board1 = new Board();
+            board1.SetHorizontally(WinnerColor, 37, 4);
+            yield return new object[] { board1 };
+
+            var board2 = new Board();
+            board2.SetHorizontally(WinnerColor, 1, 5);
+            yield return new object[] { board2 };
+
+            var board3 = new Board();
+            board3.SetVertically(WinnerColor, 10, 4);
+            board3.SetHorizontally(WinnerColor, 22, 5);
+            yield return new object[] { board3 };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetHorizontallyWinnerBoards))]
+        public void Should_BeWon_When_FourInARowHorizontally(Board board)
+        {
+            //Arrange
+            var analyzer = new Analyzer(board);
+
+            //Act
+            bool isWon = analyzer.HasWon(WinnerColor);
+
+            //Assert
+            Assert.True(isWon);
+        }
+
+        [Fact]
+        public void Should_NotBeWon_When_Empty()
+        {
+            //Arrange
+            var analyzer = new Analyzer(new Board());
+
+            //Act
+            bool isWon = analyzer.HasWon(WinnerColor);
+
+            //Assert
+            Assert.False(isWon);
+        }
+
+        [Fact]
+        public void Should_NotBeWon_When_FourSideBySideInTwoRows()
+        {
+            //Arrange
+            var board = new Board();
+            board.SetHorizontally(WinnerColor, 12, 4);
+            var analyzer = new Analyzer(board);
+
+            //Act
+            bool isWon = analyzer.HasWon(WinnerColor);
+
+            //Assert
+            Assert.False(isWon);
+        }
+
+        public static IEnumerable<object[]> GetVerticallyWinnerBoards()
+        {
+            var board1 = new Board();
+            board1.SetVertically(WinnerColor, 1, 4);
+            yield return new object[] { board1 };
+
+            var board2 = new Board();
+            board2.SetVertically(WinnerColor, 1, 5);
+            yield return new object[] { board2 };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetVerticallyWinnerBoards))]
+        public void Should_BeWon_When_FourInARowVertically(Board board)
+        {
+            //Arrange
+            var analyzer = new Analyzer(board);
+
+            //Act
+            bool isWon = analyzer.HasWon(WinnerColor);
+
+            //Assert
+            Assert.True(isWon);
+        }
+
+        public static IEnumerable<object[]> GetDiagonallyWinnerBoards()
+        {
+            var board1 = new Board();
+            board1.Set(WinnerColor, new int[] { 0, 8, 16, 24 });
+            yield return new object[] { board1 };
+
+            var board2 = new Board();
+            board2.Set(WinnerColor, new int[] { 9, 17, 25, 33, 41 });
+            yield return new object[] { board2 };
+
+            var board3 = new Board();
+            board3.Set(WinnerColor, new int[] { 3, 9, 15, 21 });
+            yield return new object[] { board3 };
+
+            var board4 = new Board();
+            board4.Set(WinnerColor, new int[] { 6, 12, 18, 24, 30, 36 });
+            yield return new object[] { board4 };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetDiagonallyWinnerBoards))]
+        public void Should_BeWon_When_FourAdjacentADiagonally(Board board)
+        {
+            //Arrange
+            var analyzer = new Analyzer(board);
+
+            //Act
+            bool isWon = analyzer.HasWon(WinnerColor);
+
+            //Assert
+            Assert.True(isWon);
+        }
+    }
+}
---
+++ connect4.test/BoardHelper.cs
@@ -0,0 +1,32 @@
+﻿using connect4.lib;
+using System.Collections.Generic;
+
+namespace connect4.test
+{
+    public static class BoardHelper
+    {
+        public static void SetHorizontally(this Board board, BoardCellState state, int startIndex, int count)
+        {
+            for (int i = startIndex; i< startIndex + count; i++)
+            {
+                board.States[i] = state;
+            }
+        }
+
+        public static void SetVertically(this Board board, BoardCellState state, int startIndex, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                board.States[startIndex+i*Board.Columns] = state;
+            }
+        }
+
+        public static void Set(this Board board, BoardCellState state, IEnumerable<int> indices)
+        {
+            foreach (int index in indices)
+            {
+                board.States[index] = state;
+            }
+        }
+    }
+}
