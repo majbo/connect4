03e5cdfee624ce3d62cb45b61fcb073eb2f14674
majbo <codingmajbo@gmail.com>
2018-01-21 22:43:26 +0100

    Created analyzer for board analysis (HasWon for now)

---
+++ connect4.lib/Analyzer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace connect4.lib
+{
+    public class Analyzer
+    {
+        private Board _board;
+
+        public Analyzer(Board board)
+        {
+            _board = board;
+        }
+
+        public bool HasWon(BoardCellState winnerColor)
+        {
+            return HasWon(winnerColor, _board.GetRows())
+                || HasWon(winnerColor, _board.GetColumns())
+                || HasWon(winnerColor, _board.GetLeftDiagonals())
+                || HasWon(winnerColor, _board.GetRightDiagonals());
+        }
+
+        private bool HasWon(BoardCellState winnerColor, IEnumerable<IEnumerable<BoardCellState>> collection)
+        {
+            int adjacent = 0;
+            foreach (var outer in collection)
+            {
+                foreach (var inner in outer)
+                {
+                    if (inner == winnerColor)
+                    {
+                        adjacent++;
+                        if (adjacent == 4)
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        adjacent = 0;
+                    }
+                }
+                adjacent = 0;
+            }
+            return false;
+        }
+    }
+}
