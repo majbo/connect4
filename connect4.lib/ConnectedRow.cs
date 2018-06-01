02ce3c9247c1c2a25b48b6ffa7965dbf3dd36607
majbo <codingmajbo@gmail.com>
2018-06-01 11:20:54 +0200

    Added class ConnectedRow

---
+++ connect4.lib/ConnectedRow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace connect4.lib
+{
+    public class ConnectedRow
+    {
+        public BoardCellState BoardCellState { get; set; }
+
+        public IEnumerable<int> Indices { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var compary = obj as ConnectedRow;
+            if (compary == null
+                || compary.BoardCellState != BoardCellState
+                || (compary.Indices == null && Indices != null)
+                || (compary.Indices != null && Indices == null)) return false;
+
+            if (compary.Indices == null) return true;
+
+            var comparyArray = compary.Indices.ToArray();
+            var array = Indices.ToArray();
+            if (comparyArray.Length != array.Length) return false;
+
+            for (int i=0; i<array.Length; i++)
+            {
+                if (comparyArray[i] != array[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ connect4.test/ConnectedRowTest.cs
@@ -0,0 +1,45 @@
+﻿using connect4.lib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace connect4.test
+{
+    public class ConnectedRowTest
+    {
+        [Fact]
+        public void Should_NotBeEqual_WhenNotAConnectedRow()
+        {
+            //Arrange
+            var connectedRow = new ConnectedRow();
+            var someObject = new object();
+
+            //Act
+            bool areEqual = connectedRow.Equals(someObject);
+
+            //Assert
+            Assert.False(areEqual);
+        }
+
+        [Fact]
+        public void Should_NotBeEqual_WhenStateAreDifferent()
+        {
+            //Arrange
+            var connectedRow = new ConnectedRow
+            {
+                BoardCellState = BoardCellState.Blue
+            };
+            var otherConnectedRow = new ConnectedRow
+            {
+                BoardCellState = BoardCellState.Red
+            };
+
+            //Act
+            bool areEqual = connectedRow.Equals(otherConnectedRow);
+
+            //Assert
+            Assert.False(areEqual);
+        }
+    }
+}
